4bdb929f0b2a0b5402dc9e7405a3b8baffea7ccc
basilcea <ogbonna.basil3@gmmail.com>
2019-08-19 22:40:30 +0100

    finish writing test for redux state

--- src/redux/actionCreators.test.js
+++ src/redux/actionCreators.test.js
@@ -2,11 +2,14 @@ import * as actions from './actionCreators';
 import * as types from './actionTypes';
 import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
+import axios from 'axios';
+import axiosMock from 'axios-mock-adapter';
 
 describe('fetch locations', () => {
-  afterEach(() => {
-    fetchMock.restore()
-  })
+const mock = new axiosMock(axios)
+const middlewares = [thunk]
+let url;
+const mockStore = configureMockStore(middlewares)
     const locations=[{
         id:1,
         description:'123 Arizona road',
@@ -30,9 +33,34 @@ describe('fetch locations', () => {
       expect(actions.locationSuccess(locations)).not.toEqual(expectedAction)
       expect(actions.locationFailure(error)).toEqual(expectedAction)
     })
-    it('location_loading success',()=>{
-      fetchMock.getOnce('/locations')
-
+    it('location_loading success', async ()=>{
+      mock.onGet(`${url}/locations`).reply(200, locations);
+      const expectedActions = [
+        { type: types.LOADING_LOCATIONS },
+        { type: types.FETCH_LOCATIONS_SUCCESS, payload:locations }
+      ]
+      const store = mockStore({ locations:[] })
+      await store.dispatch(actions.locationLoads())
+        expect(store.getActions()).toEqual(expectedActions)
     })
-    it('location_loading failure',() => {})
+    it('location_loading failure',async() => {
+      mock.onGet(`${url}/locations`).reply(404);
+      const expectedActions =[
+        { type: types.LOADING_LOCATIONS },
+        { type: types.FETCH_LOCATIONS_FAILURE, payload:'Request failed with status code 404' }
+      ]
+      const store = mockStore({ locations:[] })
+      await store.dispatch(actions.locationLoads())
+        expect(store.getActions()).toEqual(expectedActions)
+    })
+    it('location_loading failure',async() => {
+      mock.onGet(`${url}/locations`).networkError();
+      const expectedActions =[
+        { type: types.LOADING_LOCATIONS },
+        { type: types.FETCH_LOCATIONS_FAILURE, payload:'Network Error' }
+      ]
+      const store = mockStore({ locations })
+      await store.dispatch(actions.locationLoads())
+        expect(store.getActions()).toEqual(expectedActions)
+    })
   })
