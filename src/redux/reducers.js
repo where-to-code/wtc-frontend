668df01d74dc13e6ec26afdf2bbc4d22b6f8c182
Nabeelah <nabeelahyousuph@gmail.com>
2019-08-20 14:57:39 +0100

    Formatted code

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,16 +1,17 @@
 import * as types from './actionTypes';
 
 export const locationReducer = (
-    state ={loadingLocation: false,locations:[],error:null},
-    action) =>{
-        switch(action.type){
-            case types.LOADING_LOCATIONS:
-                return {...state, loadingLocation:true}
-            case types.FETCH_LOCATIONS_SUCCESS:
-                return {...state ,loadingLocation:false, locations:action.payload }
-            case types.FETCH_LOCATIONS_FAILURE:
-                return {...state,loadingLocation:false, error:action.payload}
-            default:
-                return state
-        }
-    }
+  state = { loadingLocation: false, locations: [], error: null },
+  action
+) => {
+  switch (action.type) {
+    case types.LOADING_LOCATIONS:
+      return { ...state, loadingLocation: true };
+    case types.FETCH_LOCATIONS_SUCCESS:
+      return { ...state, loadingLocation: false, locations: action.payload };
+    case types.FETCH_LOCATIONS_FAILURE:
+      return { ...state, loadingLocation: false, error: action.payload };
+    default:
+      return state;
+  }
+};
--- src/redux/reducers.test.js
+++ src/redux/reducers.test.js
@@ -1,60 +1,76 @@
-import {locationReducer} from './reducers';
+import { locationReducer } from './reducers';
 import * as types from './actionTypes';
 
-describe('location reducer',()=>{
-    const locations=[{
-        id:1,
-        description:'123 Arizona road',
-        name: "Ariz Coffee Shop",
-        longitude:'0.999923',
-        latitude:'0.273444'
-    }]
-    it('should return the initial state',()=>{
-        expect(locationReducer(undefined, {})).toEqual(
-            {
-              loadingLocation: false,
-              locations: [],
-              error: null
-            }
-          )
-    })
-    it('should load locations',()=>{
-        expect(locationReducer({loadingLocation:false}, {
-            type:types.LOADING_LOCATIONS
-        })).toEqual(
-            {
-              loadingLocation: true
-            }
-          )
-        // test the spread state functionality
-        expect(locationReducer({loadingLocation:false,locations:[],error:null}, {
-            type:types.LOADING_LOCATIONS
-        })).toEqual(
-            {
-              loadingLocation: true,
-              locations: [],
-              error: null
-            }
-          )
-    })
-    it('should get all locations on success',() => {
-        expect(locationReducer({loadingLocation:false,locations:[],error:null}, {
-            type:types.FETCH_LOCATIONS_SUCCESS,
-            payload:locations,
-        })).toEqual({
-            loadingLocation: false,
-            locations,
-            error: null
-        })
-    })
-    it('should show error on failure',() => {
-        expect(locationReducer({loadingLocation:false,locations:[],error:null}, {
-            type:types.FETCH_LOCATIONS_FAILURE,
-            payload:'There was an error',
-        })).toEqual({
-            loadingLocation: false,
-            locations:[],
-            error: "There was an error"
-        })
-    })
-})
+describe('location reducer', () => {
+  const locations = [
+    {
+      id: 1,
+      description: '123 Arizona road',
+      name: 'Ariz Coffee Shop',
+      longitude: '0.999923',
+      latitude: '0.273444'
+    }
+  ];
+  it('should return the initial state', () => {
+    expect(locationReducer(undefined, {})).toEqual({
+      loadingLocation: false,
+      locations: [],
+      error: null
+    });
+  });
+  it('should load locations', () => {
+    expect(
+      locationReducer(
+        { loadingLocation: false },
+        {
+          type: types.LOADING_LOCATIONS
+        }
+      )
+    ).toEqual({
+      loadingLocation: true
+    });
+    // test the spread state functionality
+    expect(
+      locationReducer(
+        { loadingLocation: false, locations: [], error: null },
+        {
+          type: types.LOADING_LOCATIONS
+        }
+      )
+    ).toEqual({
+      loadingLocation: true,
+      locations: [],
+      error: null
+    });
+  });
+  it('should get all locations on success', () => {
+    expect(
+      locationReducer(
+        { loadingLocation: false, locations: [], error: null },
+        {
+          type: types.FETCH_LOCATIONS_SUCCESS,
+          payload: locations
+        }
+      )
+    ).toEqual({
+      loadingLocation: false,
+      locations,
+      error: null
+    });
+  });
+  it('should show error on failure', () => {
+    expect(
+      locationReducer(
+        { loadingLocation: false, locations: [], error: null },
+        {
+          type: types.FETCH_LOCATIONS_FAILURE,
+          payload: 'There was an error'
+        }
+      )
+    ).toEqual({
+      loadingLocation: false,
+      locations: [],
+      error: 'There was an error'
+    });
+  });
+});
