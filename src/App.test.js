3c0ff1bdb8234dbf48c0deb97944b56bd114c07e
basilcea <ogbonna.basil3@gmmail.com>
2019-08-20 21:21:40 +0100

    mock store in tests to fix  coveralls error

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from "react-redux";
-import {createStore} from 'redux';
-import {locationReducer} from './redux/reducers'
-
+import configureMockStore from 'redux-mock-store';
+const initialState ={
+  loading:false
+}
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={createStore(locationReducer)}><App /></Provider>, div);
+  ReactDOM.render(<Provider store={configureMockStore()(initialState)}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
