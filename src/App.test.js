14fcf62dc93b4377288b0f78269d1fdd18f0fd9c
basilcea <ogbonna.basil3@gmmail.com>
2019-08-20 22:33:55 +0100

    fix coverall errors

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from "react-redux";
+import "react-redux";
 import configureMockStore from 'redux-mock-store';
 const initialState ={
   loading:false
 }
+const Provide =jest.genMockFromModule('react-redux');
+const Provider = Provide.Provider
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Provider store={configureMockStore()(initialState)}><App /></Provider>, div);
