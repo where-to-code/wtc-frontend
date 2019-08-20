6514e5bb0b1fc2434fb0c4542fe00db4dddfc69e
Nabeelah <nabeelahyousuph@gmail.com>
2019-08-20 14:59:31 +0100

    Create action creator to load map api - connect app js to redux store

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -2,22 +2,25 @@ import * as types from './actionTypes';
 import axios from 'axios';
 let url;
 export const locationSuccess = locationList => ({
-    type: types.FETCH_LOCATIONS_SUCCESS,
-    payload: locationList,
-})
+  type: types.FETCH_LOCATIONS_SUCCESS,
+  payload: locationList
+});
 
 export const locationFailure = error => ({
-    type: types.FETCH_LOCATIONS_FAILURE,
-    payload: error,
-})
+  type: types.FETCH_LOCATIONS_FAILURE,
+  payload: error
+});
 
 export const locationLoads = () => async dispatch => {
-    dispatch({type: types.LOADING_LOCATIONS})
-    try {
-        const locationsInfo = await axios.get(`${url}/locations`)
-        dispatch(locationSuccess(locationsInfo.data))
-    }
-    catch(error) {
-        dispatch(locationFailure(error.message));
-    }
-}
+  dispatch({ type: types.LOADING_LOCATIONS });
+  try {
+    const locationsInfo = await axios.get(`${url}/locations`);
+    dispatch(locationSuccess(locationsInfo.data));
+  } catch (error) {
+    dispatch(locationFailure(error.message));
+  }
+};
+
+export const loadMapApi = () => async dispatch => {
+    console.log('loading')
+};
