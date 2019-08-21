a231534e1919e6562a9b6784f5cd6a22a48ce7d7
basilcea <ogbonna.basil3@gmmail.com>
2019-08-21 12:04:30 +0100

    fix jest-test-coverage

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import "react-redux";
+import {Provider} from "react-redux";
 import configureMockStore from 'redux-mock-store';
 const initialState ={
   loading:false
 }
-const Provide =jest.genMockFromModule('react-redux');
-const Provider = Provide.Provider
+// const Provide =jest.genMockFromModule('react-redux');
+// const Provider = Provide.Provider
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Provider store={configureMockStore()(initialState)}><App /></Provider>, div);
