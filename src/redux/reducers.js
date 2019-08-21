1920a6804e4ce3dcace19a89dfe50a691690baa1
GianBarGian <giacomobenati@hotmail.it>
2019-08-21 15:15:12 +0200

    finished the maps  reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -16,6 +16,15 @@ export const locationReducer = (
   }
 };
 
-export const mapsReducer = (state = null, action) => {
-  
+export const mapsReducer = (state = { loadingMaps: false, mapsObj: null, error: null }, action) => {
+  switch (action.type) {
+    case types.LOADING_MAP_API:
+      return { ...state, loadingMaps: true };
+    case types.FETCH_MAP_API_SUCCESS:
+      return {...state, loadingLocation: false, mapsObj: action.payload};
+    case types.FETCH_MAP_API_FAILURE:
+      return {...state, loadingLocation: false, error: action.payload}
+    default:
+      return state;
+  }
 }
