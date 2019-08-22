57280f0872edef7908f03341a73ff6b714a21f0c
Nabeelah <nabeelahyousuph@gmail.com>
2019-08-22 23:19:52 +0100

    Refactor code

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -2,26 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Map from './Map';
 import Header from './Header';
-import { StyledSearch, CardContainer } from './componentStyles/SearchPageStyles';
+import {
+  StyledSearch,
+  CardContainer
+} from './componentStyles/SearchPageStyles';
 import FilterPane from './FilterPane';
 import { locationLoads } from '../redux/actionCreators';
 import LocationCard from './LocationCard';
 const SearchPage = props => {
-  const { locations, loadingLocation, locationLoads } = props;
+  const { locations } = props;
   const [places, setPlaces] = useState([]);
 
   useEffect(() => {
     // locationLoads();
     setPlaces(locations);
   }, []);
-  console.log(props);
+
   return (
     <div>
       <Header />
       <StyledSearch>
         <FilterPane />
         <CardContainer>
-          {loadingLocation && <div>Loading...</div>}
           {locations &&
             places.map(place => {
               return <LocationCard key={place.name} location={place} />;
