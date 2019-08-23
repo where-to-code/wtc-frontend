2fc00423279e99f232a00d862bec91ca7e00902d
Nabeelah <nabeelahyousuph@gmail.com>
2019-08-23 16:15:30 +0100

    Refactor code

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,7 +8,7 @@ function Map(props) {
   const { maps, mapsLoading, locations, locationLoads } = props;
 
   let newMap;
-  const defaultPos = { lat: 51.508056, lng: -0.128056, }
+  const defaultPos = { lat: 51.508056, lng: -0.128056 };
   const mapDefaultView = () => {
     newMap = new maps.mapsObj.Map(document.getElementById('map'), {
       zoom: 15,
@@ -31,14 +31,16 @@ function Map(props) {
   };
 
   const setCenterToUserLocation = (browserHasGeolocation, newMap) => {
-    // add the marker to the center 
+    // add the marker to the center
     new maps.mapsObj.Marker({
       map: newMap,
       position: newMap.getCenter()
     });
     if (!browserHasGeolocation) {
-      console.log("this browser doesn't support geolocation or you didn't allow it. Map is centered to default position");
-    } 
+      console.log(
+        "this browser doesn't support geolocation or you didn't allow it. Map is centered to default position"
+      );
+    }
   };
 
   useEffect(() => {
@@ -50,10 +52,10 @@ function Map(props) {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        locationLoads(pos)
-      })
+        locationLoads(pos);
+      });
     } else {
-      locationLoads(defaultPos)
+      locationLoads(defaultPos);
     }
     // Then we build the map
     if (maps.mapsObj) {
@@ -61,23 +63,26 @@ function Map(props) {
     } else {
       mapsLoading();
     }
-  // }, [maps.mapsObj]);
-  // return <StyledMap id="map" />;
+    // }, [maps.mapsObj]);
+    // return <StyledMap id="map" />;
 
     // Finally we add the markers of the locations on the map
     if (locations.locations.length > 0) {
-      locations.locations.map(location => 
-        new maps.mapsObj.Marker({
-        map: newMap,
-        position: {
-          lat: parseFloat(location.latitude),
-          lng: parseFloat(location.longitude) 
-        }
-      }))
+      locations.locations.map(
+        location =>
+          new maps.mapsObj.Marker({
+            map: newMap,
+            position: {
+              lat: parseFloat(location.latitude),
+              lng: parseFloat(location.longitude)
+            }
+          })
+      );
     } else {
-      console.log('Unfortunately we have no locations to suggests around you. Would you like to add one?')
+      console.log(
+        'Unfortunately we have no locations to suggests around you. Would you like to add one?'
+      );
     }
-   
   }, [maps.mapsObj, locations.locations.length]);
   return (
     <div className="App">
@@ -94,10 +99,13 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    mapsLoading,
-    locationLoads
-  }, dispatch);
+  return bindActionCreators(
+    {
+      mapsLoading,
+      locationLoads
+    },
+    dispatch
+  );
 }
 
 export default connect(
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,44 +3,7 @@ import * as types from './actionTypes';
 export const locationReducer = (
   state = {
     loadingLocation: false,
-    locations: [
-      {
-        name: 'Al Ain Tower',
-        description: 'A Starbucks shop to work out from',
-        image_url:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2dDHB_nliR3160ubZgp6J9A3nfPUsHMXcoVGwgb4x3oBzgGNU',
-        address: 'Khaldiya Area, Abu Dhabi Island, Abu Dhabi, AZ',
-        latitude: '24.51',
-        longitude: '54.54'
-      },
-      {
-        name: 'Eastern Mangroves',
-        description: 'A Starbucks shop to work out from',
-        image_url:
-          'https://purewows3.imgix.net/images/articles/2017_01/bespoke_coworking_spaces_san_francisco.png?auto=format,compress&cs=strip',
-        address: 'Al Salam Street, The Mangroves, Abu Dhabi, AZ',
-        latitude: '24.48',
-        longitude: '54.38'
-      },
-      {
-        name: 'Yas Mall 3',
-        description: 'A Starbucks shop to work out from',
-        image_url:
-          'https://www.rent24.com/wp-content/uploads/coworking-space-berlin-schoeneberg.jpg',
-        address: 'YAS Island, Abu Dhabi, AZ, AE',
-        latitude: '24.48',
-        longitude: '54.6'
-      },
-      {
-        name: 'Al Seef Village',
-        description: 'A Starbucks shop to work out from',
-        image_url:
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiLqR1M8Oq2awl4d6DrzDOeN90CZCIHZA6M5YI6GIg77mQgH4AwQ',
-        address: 'Salam Street, Ministries District, Abu Dhabi, AZ',
-        latitude: '24.48',
-        longitude: '54.38'
-      }
-    ],
+    locations: [],
     error: null
   },
   action
