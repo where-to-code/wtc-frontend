c1a97475a72acc4aa76c760f1356c7a7563ddc24
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-28 13:47:09 +0100

    completed the markup

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,22 +1,37 @@
 import React from 'react';
 
+import {
+  StyledMap,
+  StyledLeftSection,
+  StyledRegistration,
+} from './componentStyles/RegistrationStyle';
+import gitHubIcon from '../assets/github.png';
+import logo from '../assets/logo.png';
+
 const Registration = () => {
   return (
-    <div>
-      <form>
+    <StyledRegistration>
+      <StyledLeftSection>
         <h2>Create Account</h2>
 
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <input type="text" placeholder="Confirm Password" />
-        <button type="submit">Sign Up</button>
-      </form>
-      <div>
-        <img src="../assets/logo.png" alt="Where-to-code" />
-      </div>
-    </div>
+        <form>
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+          <input type="text" placeholder="Email" />
+          <input type="text" placeholder="Password" />
+          <input type="text" placeholder="Confirm Password" />
+          <button type="submit">Sign Up</button>
+        </form>
+
+        <p>
+          Or sign up via GitHub
+        </p>
+        <img src={gitHubIcon} alt="github" />
+      </StyledLeftSection>
+      <StyledMap>
+        <img src={logo} alt="Where-to-code" />
+      </StyledMap>
+    </StyledRegistration>
   );
 };
 
