3e349c0d7042292c39eb8688496444c490ba8bd4
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-28 14:06:41 +0100

    Used hooks to hold form state

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import {
   StyledMap,
@@ -9,23 +9,70 @@ import gitHubIcon from '../assets/github.png';
 import logo from '../assets/logo.png';
 
 const Registration = () => {
+  const [formState, updateFormState] = useState({
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleChange = e =>
+    updateFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(formState);
+  };
+
   return (
     <StyledRegistration>
       <StyledLeftSection>
         <h2>Create Account</h2>
 
-        <form>
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="text" placeholder="Email" />
-          <input type="text" placeholder="Password" />
-          <input type="text" placeholder="Confirm Password" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="First Name"
+            name="firstname"
+            value={formState.firstname}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lastname"
+            value={formState.lastname}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={formState.password}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            value={formState.confirmPassword}
+            onChange={handleChange}
+          />
           <button type="submit">Sign Up</button>
         </form>
 
-        <p>
-          Or sign up via GitHub
-        </p>
+        <p>Or sign up via GitHub</p>
         <img src={gitHubIcon} alt="github" />
       </StyledLeftSection>
       <StyledMap>
