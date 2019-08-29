aba5aadc3c462854262d015967d3de29d670aa3c
GianBarGian <giacomobenati@hotmail.it>
2019-08-29 15:52:57 +0200

    commented and reorganized action creators

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const url = 'https://where2code.herokuapp.com/api';
 
+// ACTIONS FOR LOCATIONS REDUCER
 export const locationSuccess = locationList => ({
   type: types.FETCH_LOCATIONS_SUCCESS,
   payload: locationList.data
@@ -25,6 +26,7 @@ export const locationLoads = currentLocation => async dispatch => {
   }
 };
 
+// ACTIONS FOR MAPS REDUCER
 export const mapsSucces = (mapsObj, geolocation) => ({
   type: types.FETCH_MAP_API_SUCCESS,
   payload: { mapsObj, geolocation }
@@ -57,7 +59,16 @@ export const mapsLoading = geolocation => async dispatch => {
   }
 };
 
-// Actions to get Single location data
+export const setGeolocationTrue = () => ({
+  type: types.SET_GEOLOCATION_TRUE
+})
+
+export const setGeolocationFalse = () => ({
+  type: types.SET_GEOLOCATION_FALSE
+})
+
+
+// ACTIONS FOR SINGLE LOCATION REDUCER
 export const singleLocSuccess = locationList => ({
   type: types.FETCH_SINGLE_LOCATIONS_SUCCESS,
   payload: locationList
@@ -78,17 +89,8 @@ export const fetchSingleLocation = locId => async dispatch => {
   }
 };
 
+// ACTIONS FOR ACTIVE LOCATION REDUCER
 export const setActive = location => ({
   type: types.SET_ACTIVE,
   payload: location
 })
-
-export const setGeolocationTrue = () => ({
-  type: types.SET_GEOLOCATION_TRUE
-})
-
-export const setGeolocationFalse = () => ({
-  type: types.SET_GEOLOCATION_FALSE
-})
-
-
