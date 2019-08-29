5c2e2bfbd76cc2c9a8f1470daef344df144b5732
GianBarGian <giacomobenati@hotmail.it>
2019-08-29 10:43:33 +0200

    removed Redux Dev Tools

--- src/index.js
+++ src/index.js
@@ -24,8 +24,7 @@ const store = createStore(
   combinedReducer,
   {},
   compose(
-    applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    
+    applyMiddleware(thunk),
   )
 );
 
