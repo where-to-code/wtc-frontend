cdc72cd96fb3f5acb0ae0f02ca8f8aedc63010a6
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-30 11:37:01 +0100

    Changed the url to staging

--- src/views/GitHubAuth.js
+++ src/views/GitHubAuth.js
@@ -4,7 +4,8 @@ import { Redirect } from 'react-router-dom';
 import Loader from 'react-loader-spinner';
 import styled from 'styled-components';
 
-const url = 'https://where2code.herokuapp.com/api/auth/gitAuth';
+// const url = 'https://where2code.herokuapp.com/api/auth/gitAuth';
+const url = 'https://where-to-code-staging.herokuapp.com/auth/gitAuth'
 
 const StyledSpinner = styled.div`
   display: flex;
