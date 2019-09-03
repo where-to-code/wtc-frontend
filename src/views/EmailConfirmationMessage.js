a92228dd6090062e7515ed9c6327b9768406ccc1
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-03 11:00:23 +0100

    Completed the page

--- src/views/EmailConfirmationMessage.js
+++ src/views/EmailConfirmationMessage.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
-import thankYou from '../assets/thank-you.svg';
+import thankYou from '../assets/thanks.svg';
+import {
+  StyledMessage,
+  StyledConfirmationPage,
+} from './ViewStyles/EmailConfirmationMessageStyles';
 
 export default () => (
-  <>
+  <StyledConfirmationPage>
     <Header />
-    <div>
+    <StyledMessage>
       <img src={thankYou} alt="thank_you" />
-      <p>Your email has been confirmed successfully</p>
+      <p>Your email has been confirmed successfully.</p>
       <Link to="/">
         <button>Continue</button>
       </Link>
-    </div>
-  </>
+    </StyledMessage>
+  </StyledConfirmationPage>
 );
