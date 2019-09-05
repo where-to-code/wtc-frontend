d5450d337bca63678318befeb92fa172d04a21a8
Haja Andriamaro <haja.andri@gmail.com>
2019-09-05 14:07:24 +0200

    final styling of overlay message with toggled display

--- src/components/TopNotif.js
+++ src/components/TopNotif.js
@@ -3,7 +3,7 @@ import { StyledOverlayPopup } from '../components/componentStyles/OverlayPopupSt
 
 const TopNotif = props => {
     const showMessage = () =>{
-        document.getElementById('popup').style.display = 'block';
+        document.getElementById('popup').style.display = 'flex';
     }
     const hideMessage = () =>{
         document.getElementById('popup').style.display = 'none';
@@ -15,12 +15,18 @@ const TopNotif = props => {
         onClick={showMessage}>
         <div>?</div>
         </div>
+
             <StyledOverlayPopup id="popup">
-            <p>Please verify your email</p>
-            <p>We were not able to verify your email</p>
-            <p>Please check your mail box and follow the instruction</p>
-            <div onClick={hideMessage}>Ok</div>
+            <div className="message-container">
+            <h3>Please verify your email</h3>
+            <p>We have not been able to verify your email yet. Please check your mail box and follow the instruction</p>
+            <div className="actions-row">
+                <div className="ok" onClick={hideMessage}>OK</div>
+                <div className="resend" onClick={hideMessage}>Resend me a verification email</div>
+            </div>            
+            </div>
             </StyledOverlayPopup>
+
       </>
     );
 };
--- src/components/componentStyles/OverlayPopupStyles.js
+++ src/components/componentStyles/OverlayPopupStyles.js
@@ -1,17 +1,50 @@
 import styled from 'styled-components';
 
 export const StyledOverlayPopup = styled.div`
-display: none;
-position: fixed;
-padding: 10px;
-width: 280px;
-left: 50%;
-margin-left: -150px;
-height: 180px;
-top: 50%;
-margin-top: -100px;
-background: #FFF;
-z-index: 20;
--webkit-box-shadow:  0px 0px 0px 9999px rgba(0, 0, 0, 0.5);
-box-shadow:  0px 0px 0px 9999px rgba(0, 0, 0, 0.5);
+position: fixed; /* Sit on top of the page content */
+display: none; /* Hidden by default */;
+justify-content: center;
+align-items: center;
+text-align:center;
+width: 100%; /* Full width (cover the whole page) */
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
+background-color: rgba(0,0,0,0.5); /* Black background with opacity */
+z-index: 2; /* Specify a stack order in case you're using a different order for other elements */
+cursor: pointer; /* Add a pointer on hover */
+.message-container{
+    padding: 20px;
+    border:1px solid #E5E5E5;
+    text-align:center;
+    background:white;
+    width: 50%;
+    min-width: 300px;
+    border-radius:10px;
+    color: #666A7C;
+}
+h3{
+    font-size: 1.5rem;
+    padding-bottom:20px;
+    color: #03525B;
+}
+.actions-row{
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+}
+.ok, .resend{
+    font-size: 1rem;
+    padding:15px;
+    border:1px solid #56C1CB;
+    border-radius: 5px;
+    color: #03525B;
+}
+.ok{
+    border:none;
+    font-weight:bold;
+}
+
 `;
