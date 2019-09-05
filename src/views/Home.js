76c06aa940c9716f54d5e8b1d6bb4c00a2c3f7d3
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-05 17:50:50 +0100

    Added  a map loading action creator

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -7,26 +7,67 @@ import { Redirect } from 'react-router-dom';
 import { StyledHome } from './ViewStyles/HomeStyles';
 import Header from '../components/Header';
 import { mapPromise } from '../redux/helpers';
-import { setGeolocationValue, clearLocations } from '../redux/actionCreators';
+import {
+  setGeolocationValue,
+  clearLocations,
+  mapsLoading,
+  mapsSucces,
+} from '../redux/actionCreators';
 
-const Home = ({ setGeolocationValue, clearLocations }) => {
+const Home = ({
+  setGeolocationValue,
+  clearLocations,
+  mapsObj,
+  mapsLoading,
+  mapsSucces,
+}) => {
   const [pos, updatePos] = useState(false);
 
-  Promise.resolve(mapPromise).then(() => {
-    /*global google*/ // To disable any eslint 'google not defined' errors
-    const autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('mapSuggestions'),
-    );
+  // Promise.resolve(mapPromise).then(() => {
+  //   /*global google*/ // To disable any eslint 'google not defined' errors
+  //   const autocomplete = new google.maps.places.Autocomplete(
+  //     document.getElementById('mapSuggestions'),
+  //   );
+  //     // console.log(google);
+  //   autocomplete.addListener('place_changed', () => {
+  //     const place = autocomplete.getPlace();
+
+  //     const latitude = place.geometry.location.lat();
+  //     const longitude = place.geometry.location.lng();
+
+  //     setGeolocationValue({ lat: latitude, lng: longitude });
+  //     clearLocations();
+  //     updatePos(true);
+  //   });
+  // });
+
+  useEffect(() => {
+    // if (!mapsObj) {
+    //   mapsLoading();
+    // } else {
+    //   console.log(mapsObj);
+    //   mapsObj.map.places.Autocomplete(document.getElementById('mapSuggestions'));
+    // }
+
+    Promise.resolve(mapPromise).then(mapObject => {
+      /*global google*/ // To disable any eslint 'google not defined' errors
+      // mapsSucces(mapObject.maps);
+      // console.log(mapObject.maps)
+
+      const autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('mapSuggestions'),
+      );
       // console.log(google);
-    autocomplete.addListener('place_changed', () => {
-      const place = autocomplete.getPlace();
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
 
-      const latitude = place.geometry.location.lat();
-      const longitude = place.geometry.location.lng();
+        const latitude = place.geometry.location.lat();
+        const longitude = place.geometry.location.lng();
 
-      setGeolocationValue({ lat: latitude, lng: longitude });
-      clearLocations();
-      updatePos(true);
+        setGeolocationValue({ lat: latitude, lng: longitude });
+        clearLocations();
+        updatePos(true);
+      });
     });
   });
 
@@ -38,7 +79,11 @@ const Home = ({ setGeolocationValue, clearLocations }) => {
       <div className="container">
         <h2>Find the best places to code</h2>
         <form type="submit">
-          <input type="text" placeholder="Search for places" id="mapSuggestions" />
+          <input
+            type="text"
+            placeholder="Search for places"
+            id="mapSuggestions"
+          />
           {/* <input type="submit" value="" /> */}
         </form>
         <Link to="/locations">
@@ -49,17 +94,25 @@ const Home = ({ setGeolocationValue, clearLocations }) => {
   );
 };
 
+function mapStateToProps(state) {
+  return {
+    mapsObj: state.maps.mapsObj,
+  };
+}
+
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(
     {
       setGeolocationValue,
-      clearLocations
+      clearLocations,
+      mapsLoading,
+      mapsSucces,
     },
     dispatch,
   );
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(Home);
