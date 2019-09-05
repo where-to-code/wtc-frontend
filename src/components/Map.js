ef3f33fae104dfb7cb25431bd249fa2053a05113
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-05 20:12:27 +0100

    Began refactoring the map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,9 +2,15 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { StyledMap } from './componentStyles/MapStyles';
-import { mapsLoading, locationLoads, setActive, setGeolocationValue } from '../redux/actionCreators';
+import {
+  mapsLoading,
+  locationLoads,
+  setActive,
+  setGeolocationValue,
+} from '../redux/actionCreators';
 import { modalInit, markerInit, mapInit, position } from './helpers/mapHelpers';
 import markerBlue from '../assets/icons8-marker-40.png';
+import { mapPromise } from '../redux/helpers';
 
 const Map = props => {
   const {
@@ -15,7 +21,7 @@ const Map = props => {
     singleLocCoord,
     setActive,
     activeLocation,
-    setGeolocationValue
+    setGeolocationValue,
   } = props;
 
   // Set the default position to Trafalgar Square, London
@@ -24,63 +30,71 @@ const Map = props => {
   let center;
 
   if (activeLocation) {
-    center = {
+    mapCenter = {
       lat: Number(activeLocation.latitude),
       lng: Number(activeLocation.longitude),
     };
-
-    mapCenter = center;
   }
 
-  const updateView = (location) =>{
-    let elm = document.getElementById(location.id)
+  const updateView = location => {
+    let elm = document.getElementById(location.id);
     elm.scrollIntoView();
     setActive(location);
-  }
+  };
 
   useEffect(() => {
-    // we need to use a wrapper to use async functions inside UseEffect
-    const asyncWrap = async () => {
-      // we get the user Geolocation, if we can't this return the default position
-      mapCenter = await position(mapCenter);
-      let map;
-      // If we already got the mapObj we build the map
-      if (mapsObj && singleLocCoord) {
-        setGeolocationValue(singleLocCoord);
-        map = mapInit(mapsObj, geolocation);
-      } 
-      else if (mapsObj) map = mapInit(mapsObj, geolocation);
-      //Or we fetch it from google API before
-      else if (!geolocation) {
-        setGeolocationValue(mapCenter);
-        mapsLoading();
-      }
-      else {
-        mapsLoading();
-      } 
+    Promise.resolve(mapPromise).then(async mapObject => {
+      // we need to use a wrapper to use async functions inside UseEffect
+      // const asyncWrap = async () => {
+        // we get the user Geolocation, if we can't this return the default position
 
-      // if (!geolocation) setGeolocationValue(mapCenter);
-    
-      // We add markers and modals to locations
-      if (locations.length > 0) {
-        locations.map(location => {
-          let marker;
-          const selectedLocation =
-            activeLocation && activeLocation.name === location.name;
+        let map;
+        mapCenter = await position(mapCenter);
 
-          if (selectedLocation) {
-            const modal = modalInit(mapsObj, location);
-            marker = markerInit(map, mapsObj, location);
-            modal.open(map, marker);
-          } else {
-            marker = markerInit(map, mapsObj, location, markerBlue);
-          }
-          marker.addListener('click', () => updateView(location));
-        });
-      }
-    };
-    asyncWrap();
-  }, [activeLocation, locations.length, geolocation]);
+        if (!geolocation) {
+          map = mapInit(mapObject.maps, mapCenter);
+          setGeolocationValue(mapCenter);
+        }
+        else{
+          map = mapInit(mapObject.maps, geolocation);
+        }
+        // If we already got the mapObj we build the map
+        // if (mapObject) {
+        //   setGeolocationValue(singleLocCoord);
+          
+
+        // if (mapObject) map = mapInit(mapObject.maps, geolocation);
+        // //Or we fetch it from google API before
+        // else if (!geolocation) {
+        //   setGeolocationValue(mapCenter);
+        //   mapsLoading();
+        // } else {
+        //   mapsLoading();
+        // }
+
+        // We add markers and modals to locations
+        if (locations.length > 0) {
+          locations.map(location => {
+            let marker;
+            const selectedLocation =
+              activeLocation && activeLocation.name === location.name;
+
+            if (selectedLocation) {
+              const modal = modalInit(mapObject.maps, location);
+              marker = markerInit(map, mapObject.maps, location);
+              modal.open(map, marker);
+            } else {
+              marker = markerInit(map, mapObject.maps, location, markerBlue);
+            }
+            marker.addListener('click', () => updateView(location));
+          });
+        }
+      // };
+      // asyncWrap();
+    });
+  }, 
+  // [activeLocation, locations.length, geolocation]
+  );
 
   return <StyledMap id="map" />;
 };
@@ -100,7 +114,7 @@ function mapDispatchToProps(dispatch) {
       mapsLoading,
       locationLoads,
       setActive,
-      setGeolocationValue
+      setGeolocationValue,
     },
     dispatch,
   );
