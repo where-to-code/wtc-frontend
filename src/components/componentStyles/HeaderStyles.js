e9151eb67059ab1aca0623a7d0a9d7e3ad10aae6
Haja Andriamaro <haja.andri@gmail.com>
2019-09-05 20:51:59 +0200

    swapped top notif icon color set for more visibility

--- src/components/componentStyles/HeaderStyles.js
+++ src/components/componentStyles/HeaderStyles.js
@@ -41,13 +41,14 @@ export const StyledHeader = styled.header`
     justify-content: center;
     height: 22px;
     width: 22px;
+    background: #EB5757;
     border:1px solid #EB5757;
     border-radius: 50%;
     margin-right:10px;
     div{
       font-size: .8rem;
       font-weight: bold;
-      color:#EB5757;
+      color:white;
       text-align:center;
     }
   }
