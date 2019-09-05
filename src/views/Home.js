53ac2dc614f90ac240b0c438b5a466b98cb5ff6a
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-05 12:23:05 +0100

    Used the api to perform autocomplete

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,15 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { StyledHome } from './ViewStyles/HomeStyles';
 import Header from '../components/Header';
+import { mapPromise } from '../redux/helpers';
 
 const Home = () => {
+  Promise.resolve(mapPromise).then(() => {
+    /*global google*/ // To disable any eslint 'google not defined' errors
+    const autocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('mapSuggestions'),
+    );
+  });
+
   return (
     <StyledHome>
-      <Header landing={true}/>
+      <Header landing={true} />
       <div className="container">
         <h2>Find the best places to code</h2>
         <form type="submit">
-          <input type="text" placeholder="Search" />
+          <input type="text" placeholder="Search" id="mapSuggestions" />
           <input type="submit" value="" />
         </form>
         <Link to="/locations">
