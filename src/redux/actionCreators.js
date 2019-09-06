a9634967fa3807b9ae8c0dc0130c65ce7eef19ee
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-06 10:34:08 +0100

    Refactored the action creators

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,47 +1,46 @@
 import * as types from './actionTypes';
 import axios from 'axios';
-import { mapPromise } from './helpers';
-import { toast } from "react-toastify";
-
+// import { mapPromise } from './helpers';
 
 const url = 'https://where2code.herokuapp.com/api';
-
 
 // Auth
 export function authLoad() {
   return {
-    type: types.AUTH_LOAD
+    type: types.AUTH_LOAD,
   };
 }
 
 export function authSuccess(user) {
   return {
     type: types.AUTH_SUCCESS,
-    payload: user
+    payload: user,
   };
 }
 
 export function authFail(payload) {
   return {
     type: types.AUTH_FAILURE,
-    payload: payload
+    payload: payload,
   };
 }
 
 export const login = user => async dispatch => {
   dispatch(authLoad());
   try {
-    const loginDetails = await axios.post(`${url}/auth/login`, user, {
-      withCredentials: true
-    })
-    .then(res => {
-      dispatch(authSuccess(res.data.data));
-      return res;
-    })
-    .catch(err => {
-      dispatch(authFail(err.response.data.message));
-      return err;
-    });
+    const loginDetails = await axios
+      .post(`${url}/auth/login`, user, {
+        withCredentials: true,
+      })
+      .then(res => {
+        dispatch(authSuccess(res.data.data.id));
+        return res;
+      })
+      .catch(err => {
+        dispatch(authFail(err.response.data.message));
+        return err;
+      });
+    dispatch(authSuccess(loginDetails.data.data.id));
     return loginDetails;
   } catch (error) {
     dispatch(authFail(error.response.data.message));
@@ -49,29 +48,35 @@ export const login = user => async dispatch => {
   }
 };
 
-export const successGitlog = (userData) => dispatch =>{
-  dispatch(authSuccess(userData));
+export const successGitlog = userId => dispatch => {
+  dispatch(authSuccess(userId));
 };
 
 export const signup = userData => async dispatch => {
   const { firstname, lastname, email, password } = userData;
   dispatch(authLoad());
   try {
-    const userDetails = await axios.post(`${url}/auth/register`, {
-      firstname,
-      lastname,
-      email,
-      password
-    }, { withCredentials: true } )
-    .then(res => {
-      dispatch(authSuccess(res.data.data));
-      return res;
-    })
-    .catch(err => {
-      console.log(err);
-      dispatch(authFail(err.response.data.message));
-      return err;
-    });
+    const userDetails = await axios
+      .post(
+        `${url}/auth/register`,
+        {
+          firstname,
+          lastname,
+          email,
+          password,
+        },
+        { withCredentials: true },
+      )
+      .then(res => {
+        dispatch(authSuccess(res.data.data.id));
+        return res;
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err.response.data.message));
+        return err;
+      });
+    dispatch(authSuccess(userDetails.data.data.id));
     return userDetails;
   } catch (error) {
     dispatch(authFail(error.response.data.message));
@@ -82,36 +87,41 @@ export const signup = userData => async dispatch => {
 // Locations
 export const locationSuccess = locationList => ({
   type: types.FETCH_LOCATIONS_SUCCESS,
-  payload: locationList.data
+  payload: locationList.data,
 });
+
 export const locationFailure = error => ({
   type: types.FETCH_LOCATIONS_FAILURE,
-  payload: error
+  payload: error,
 });
+
 export const locationLoads = currentLocation => async dispatch => {
   dispatch({ type: types.LOADING_LOCATIONS });
   try {
     const locationsInfo = await axios.get(
-      `${url}/locations?lat=${currentLocation.lat}&long=${currentLocation.lng}`
+      `${url}/locations?lat=${currentLocation.lat}&long=${currentLocation.lng}`,
     );
     dispatch(locationSuccess(locationsInfo.data));
   } catch (error) {
     dispatch(locationFailure(error.message));
   }
 };
+
 export const clearLocations = () => ({
-  type: types.CLEAR_LOCATIONS
+  type: types.CLEAR_LOCATIONS,
 });
 
 // ACTIONS FOR MAPS REDUCER
-export const mapsSucces = mapsObj => ({
-  type: types.FETCH_MAP_API_SUCCESS,
-  payload: mapsObj
-});
+// export const mapsSucces = mapsObj => ({
+//   type: types.FETCH_MAP_API_SUCCESS,
+//   payload: mapsObj
+// });
+
 export const mapsFailure = error => ({
   type: types.FETCH_MAP_API_FAILURE,
-  payload: error
+  payload: error,
 });
+
 export const mapsLoading = () => async dispatch => {
   dispatch({ type: types.LOADING_MAP_API });
   // try {
@@ -123,26 +133,29 @@ export const mapsLoading = () => async dispatch => {
   //   dispatch(mapsFailure(error.message));
   // }
 };
+
 export const setGeolocationTrue = () => ({
-  type: types.SET_GEOLOCATION_TRUE
+  type: types.SET_GEOLOCATION_TRUE,
 });
+
 export const setGeolocationFalse = () => ({
-  type: types.SET_GEOLOCATION_FALSE
+  type: types.SET_GEOLOCATION_FALSE,
 });
+
 export const setGeolocationValue = geolocation => ({
   type: types.SET_GEOLOCATION_VALUE,
-  payload: geolocation
-})
+  payload: geolocation,
+});
 
 // ACTIONS FOR SINGLE LOCATION REDUCER
 export const singleLocSuccess = locationList => ({
   type: types.FETCH_SINGLE_LOCATIONS_SUCCESS,
-  payload: locationList
+  payload: locationList,
 });
 
 export const singleLocFailure = error => ({
   type: types.FETCH_SINGLE_LOCATIONS_FAILURE,
-  payload: error
+  payload: error,
 });
 
 export const fetchSingleLocation = locId => async dispatch => {
@@ -158,41 +171,39 @@ export const fetchSingleLocation = locId => async dispatch => {
 // ACTIONS FOR ACTIVE LOCATION REDUCER
 export const setActive = location => ({
   type: types.SET_ACTIVE,
-  payload: location
+  payload: location,
 });
+
 export const clearActive = location => ({
-  type: types.CLEAR_ACTIVE
+  type: types.CLEAR_ACTIVE,
 });
 
 // verify email
 export function verifyEmailLoad() {
   return {
-    type: types.VERIFY_EMAIL_LOAD
+    type: types.VERIFY_EMAIL_LOAD,
   };
 }
 
 export function verifyEmailSuccess(email) {
   return {
     type: types.VERIFY_EMAIL_SUCCESS,
-    payload: email
+    payload: email,
   };
 }
 
 export function verifyEmailFail(payload) {
   return {
     type: types.VERIFY_EMAIL_FAILURE,
-    payload: payload
+    payload: payload,
   };
 }
-
-
-
 
 export const verifyEmail = email => dispatch => {
   dispatch(verifyEmailLoad());
   return axios
     .post(`${url}/auth/forgot`, email, {
-      withCredentials: true
+      withCredentials: true,
     })
     .then(res => {
       dispatch(verifyEmailSuccess(res.data));
@@ -206,11 +217,10 @@ export const verifyEmail = email => dispatch => {
     });
 };
 
-
 // password reset
 export function resetPasswordLoad() {
   return {
-    type: types.RESET_PASSWORD_LOAD
+    type: types.RESET_PASSWORD_LOAD,
   };
 }
 
@@ -224,19 +234,23 @@ export function resetPasswordSuccess(password) {
 export function resetPasswordFail(payload) {
   return {
     type: types.RESET_PASSWORD_FAILURE,
-    payload: payload
+    payload: payload,
   };
 }
 
-
 export const resetPassword = (password, id) => dispatch => {
-  // dispatch(resetPasswordLoad());
-  console.log(password , id)
+  dispatch(resetPasswordLoad());
+  // console.log(password, id);
   return axios
-    .post(`${url}/auth/change/${id}`, {password}, {
-      withCredentials: true
-    })
+    .post(
+      `${url}/auth/change/${id}`,
+      { password },
+      {
+        withCredentials: true,
+      },
+    )
     .then(res => {
+      console.log('creator', res);
       dispatch(resetPasswordSuccess(res.data));
       toast.success("password reset successful, please login!");
       return res;
