07aa1ba308c5211a6ba91f4e5ef055ad54c03802
GianBarGian <giacomobenati@hotmail.it>
2019-09-06 14:52:22 +0200

    wip

--- src/views/tests/SearchPage.test.js
+++ src/views/tests/SearchPage.test.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
+import { cleanup, } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { renderWithRedux } from '../../utils/testHelpers';
-import SearchPage from '../SearchPage'
+import { renderWithRedux, mockedState } from '../../utils/testHelpers';
+import SearchPage from '../SearchPage';
+
+
+const initialState = mockedState;
+jest.mock('../../components/Header', () => () => 'Header Mock');
+jest.mock('../../components/FilterPane', () => ({ toggle = false }) => toggle ? 'FilterPane Mock hidden' : 'FilterPane Mock shown')
 
 afterEach(cleanup);
 describe('Search Page tests', () => {
     it('render without crashing', () => {
-        renderWithRedux(<SearchPage/>)
+        renderWithRedux(<SearchPage />)
     })
-    
+    it('render the loader while the locations are loading', () => {
+        const { debug } = renderWithRedux(<SearchPage />)
+        console.log(initialState)
+        debug()
+    })
 })
