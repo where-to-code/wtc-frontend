72cac243f9dab3a68fe4de20abc0f2f9db6428ed
GianBarGian <giacomobenati@hotmail.it>
2019-09-09 17:17:39 +0200

    wip

--- src/views/tests/Home.test.js
+++ src/views/tests/Home.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup, fireEvent, } from '@testing-library/react';
+import { cleanup, fireEvent, getByPlaceholderText, } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { renderWithRedux, TestingRouter } from '../../utils/testHelpers';
 import Home from '../Home';
@@ -17,10 +17,19 @@ describe('Search Page tests', () => {
         const { getByText } = renderWithRedux(<Home />)
         expect(getByText('Header Mock')).toBeTruthy();
     })
-    it('redirect to /locations if button is clicked', () => {
+    it('push to /locations if button is clicked', () => {
         const redirectUrl = '/locations'
         const { getByText, container } = renderWithRedux(<TestingRouter ComponentWithRedirection={() => <Home />} RedirectUrl={redirectUrl} />)
         fireEvent.click(getByText('Find places near you'))
+        expect(container.innerHTML).toEqual(
+            expect.stringContaining(redirectUrl)
+          )
+    })
+    it('push to /locations on autocomplete', () => {
+        const redirectUrl = '/locations'
+        const { getByPlaceholderText, container } = renderWithRedux(<TestingRouter ComponentWithRedirection={() => <Home />} RedirectUrl={redirectUrl} />)
+        fireEvent.change(getByPlaceholderText('Search and select a place'), {target: {value: 'London, UK'}})
+        fireEvent.keyPress(getByPlaceholderText('Search and select a place'), { key: 'Enter', code: 13, charCode: 13 })
         expect(container.innerHTML).toEqual(
             expect.stringContaining(redirectUrl)
           )
