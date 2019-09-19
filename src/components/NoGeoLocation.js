1bba55d9779e6ded271648cba2d2075a8334be82
oyekunle-mark <oyekunlemac@gmail.com>
2019-09-19 14:26:33 +0100

    Track the geolocation state with a notification

--- src/components/NoGeoLocation.js
+++ src/components/NoGeoLocation.js
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyledNoGeoLocation } from './componentStyles/LocationErrStyles';
 
 export default () => {
-  const [toggleNoLoc, setToggleNoLoc] = useState(true);
+  const [toggleNoLoc, setToggleNoLoc] = useState(false);
+
   const hideNoLoc = () => setToggleNoLoc(!toggleNoLoc);
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(
+      () => setToggleNoLoc(false),
+      () => setToggleNoLoc(true),
+    );
+  });
 
   return (
     <StyledNoGeoLocation toggleNoLoc={toggleNoLoc}>
