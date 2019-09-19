7e1366c9c882a8b1f9fb9624c3236608a47c9c03
GianBarGian <giacomobenati@hotmail.it>
2019-09-19 13:48:11 +0200

    fixed a problem with box-shadow

--- src/components/componentStyles/LocationCardStyles.js
+++ src/components/componentStyles/LocationCardStyles.js
@@ -4,11 +4,10 @@ export const StyledCard = styled.div`
   margin: 10px 5px;
   width: 100%;
   height: 340px;
-  box-shadow: -1px 1px 5px 1px rgba(219, 219, 219, 1);
   background-color: rgba(219, 219, 219, 0.35);
   border-radius: 5px;
   box-shadow: ${props =>
-    props.active ? '-1px 1px 5px 5px rgba(86, 193, 203, 0.5)' : 'none'};
+    props.active ? '-1px 1px 5px 5px rgba(86, 193, 203, 0.5)' : '-1px 1px 5px 1px rgba(219, 219, 219, 1)'};
   transition: 0.2s;
   transform: ${props => (props.active ? 'scale(1.05)' : 'scale(1)')};
   
