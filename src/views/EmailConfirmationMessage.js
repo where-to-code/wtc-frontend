9a3c511fe3873e06eaf7aeca09fa72bb0633ea4e
Haja Andriamaro <haja.andri@gmail.com>
2019-09-20 12:01:18 +0200

    upgraded EmailConfirmationMessage to handle user ID received from BE and auto login the user

--- src/views/EmailConfirmationMessage.js
+++ src/views/EmailConfirmationMessage.js
@@ -7,16 +7,30 @@ import {
   StyledMessage,
   StyledConfirmationPage
 } from './ViewStyles/EmailConfirmationMessageStyles';
+import { setTempCookie } from '../components/helpers/authHelpers';
 
-export default () => (
-  <StyledConfirmationPage>
-    <Header />
-    <StyledMessage>
-      <img src={thankYou} alt="thank_you" />
-      <p>Your email has been confirmed successfully.</p>
-      <Link to="/">
-        <button>Continue to homepage</button>
-      </Link>
-    </StyledMessage>
-  </StyledConfirmationPage>
-);
+export default (props) => {
+
+  // If the user get to this it means he sucessfully 
+  // confirmed his email, so we set the temp Cookie to 
+  // get him automatically logged in. However we have 
+  // only partial data from BE that can be set to state ...
+  // require BE upgrade for security and data integrity
+  setTempCookie( {
+    id: props.match.params, 
+    isVerified: true
+  }
+  )
+  return (
+    <StyledConfirmationPage>
+      <Header />
+      <StyledMessage>
+        <img src={thankYou} alt="thank_you" />
+        <p>Your email has been confirmed successfully.</p>
+        <Link to="/">
+          <button>Continue to homepage</button>
+        </Link>
+      </StyledMessage>
+    </StyledConfirmationPage>
+  );  
+}
