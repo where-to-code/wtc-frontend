4e0e6c333a0f153a3f92287b58590f5efc3071d9
Chiazokam Echeta <chiazokamecheta@gmail.com>
2019-09-25 13:36:42 +0100

    Style add location modal

--- src/components/AddLocation.js
+++ src/components/AddLocation.js
@@ -1,200 +1,204 @@
 import React, { useState, useEffect } from 'react';
-import { Redirect } from 'react-router-dom';    
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addNewLocation, clearNewLocation, hideAddLocation, locationLoads } from '../redux/actionCreators';
+import {
+  addNewLocation,
+  clearNewLocation,
+  hideAddLocation,
+  locationLoads
+} from '../redux/actionCreators';
 import { StyledOverlayPopup } from './componentStyles/OverlayPopupStyles';
-import { 
-    StyledButton,
-    StyledInput,
-    StyledTextarea,
-    Row
- } from './componentStyles/FormElementsStyles';
+import {
+  StyledButton,
+  StyledInput,
+  StyledTextarea,
+  Row
+} from './componentStyles/FormElementsStyles';
+
+import { StyledModal } from './componentStyles/ModalStyles';
 import Loader from 'react-loader-spinner';
 import { mapPromise } from '../redux/helpers';
 
+function AddLocation(props) {
+  const {
+    addNewLocation,
+    remoteError,
+    loading,
+    isAdded,
+    clearNewLocation,
+    isShown,
+    hideAddLocation,
+    isAuth,
+    locationLoads,
+    geolocation
+  } = props;
 
-function AddLocation (props){
-    const { 
-        addNewLocation, 
-        remoteError, 
-        loading,
-        isAdded,
-        clearNewLocation,
-        isShown,
-        hideAddLocation,
-        isAuth,
-        locationLoads,
-        geolocation
-    } = props
-    const [locationPhotos, setLocationPhotos] = useState(null);
-    const [description, setDescription] = useState('');
-    const [placeData, setPlaceData] = useState(null);
-    const [formError, setFormError] = useState(null);
-    const [fromUnsplash, setFromUnsplash] = useState(false);
-    
-    const hideMessage = () =>{
-        clearNewLocation();
-        setLocationPhotos(null);
-        setDescription('');
-        setPlaceData(null);
-        setFormError(null)
-        hideAddLocation();
-        locationLoads(geolocation);
+  const [locationPhotos, setLocationPhotos] = useState(null);
+  const [description, setDescription] = useState('');
+  const [placeData, setPlaceData] = useState(null);
+  const [formError, setFormError] = useState(null);
+  const [fromUnsplash, setFromUnsplash] = useState(false);
+
+  const hideMessage = () => {
+    clearNewLocation();
+    setLocationPhotos(null);
+    setDescription('');
+    setPlaceData(null);
+    setFormError(null);
+    hideAddLocation();
+    locationLoads(geolocation);
+  };
+  const submitLocation = event => {
+    event.preventDefault();
+
+    // if fields have no data
+    if (!placeData || !description) {
+      setFormError(true);
+      return;
     }
-    const submitLocation = (event) =>{
-        event.preventDefault();
-
-        // if fields have no data
-        if(!placeData || !description) {
-            setFormError(true);
-            return;
-        } 
-        addNewLocation({
-            name: placeData.name,
-            description: description,
-            image_url: locationPhotos,
-            address: placeData.formatted_address,
-            longitude: placeData.geometry.location.lng(),
-            latitude: placeData.geometry.location.lat(),
-            place_id: placeData.place_id
-        })
-    }
-
-    const handleChange = event => {
-        setDescription(event.target.value);
-        // if any error is being displayed, we remove it if user type in again
-        setFormError(false);
-    };
-
-    const redirectToLogin = () => {
-        props.history.push('/login')
-    }
-
-    // useEffect to use the Map place API autocomplete
-    useEffect(() => {
-        Promise.resolve(mapPromise).then(mapObject => {
-        const autocomplete = new mapObject.maps.places.Autocomplete(
-            document.getElementById('location-name-field'),
-        );
-        // force auto-complete to return only business
-        autocomplete.setTypes(['establishment']);
-        autocomplete.addListener('place_changed', () => {
-            const place = autocomplete.getPlace();
-            if(place.photos && place.photos[0]){
-                setLocationPhotos(place.photos[0].getUrl());
-            }
-            else {
-                // no photo was received from place API. We set random picture from Unsplash
-                // but we dont display on the form
-                setLocationPhotos('https://source.unsplash.com/1600x900/?coworking');
-                setFromUnsplash(true);
-            }
-            
-            setPlaceData(place);
-        });
-        });
+    addNewLocation({
+      name: placeData.name,
+      description: description,
+      image_url: locationPhotos,
+      address: placeData.formatted_address,
+      longitude: placeData.geometry.location.lng(),
+      latitude: placeData.geometry.location.lat(),
+      place_id: placeData.place_id
     });
+  };
 
-    return (
-        <StyledOverlayPopup isShown={isShown} id="add-location-form">
-        <div className="message-container">
-            <div className="closing-cross">
-                <span onClick={hideMessage}>X</span>
-            </div>
-            {
-                !isAuth && (
-                    <>
-                    <div>
-                    <h2>You must login first</h2>
-                    </div>
-                    <StyledButton onClick={redirectToLogin}>
-                        OK
-                    </StyledButton>
-                    </>
-                )
-            }
-            {
-                isAuth && (
-                    <>
-                    <form onSubmit={submitLocation}>
-                        <div>
-                        <h2>Add a working place</h2>
-                        </div>
-                        {
-                            isAdded 
-                            ? (
-                                <>
-                                <p> Thank you for adding this place </p>
-                                <StyledButton onClick={hideMessage}>
-                                OK
-                                </StyledButton>
-                                </>
-                            )
-                            :
-                            (
-                                <Row>
-                                    {
-                                        locationPhotos && 
-                                        (
-                                            !fromUnsplash &&
-                                            <>
-                                            <div className="left-box">
-                                            <img className="location-photos" alt="location" src={locationPhotos} />    
-                                            </div>
-                                            </>
-                                        )
-                                    }
-                                <div className="right-box">
-                                    { formError && <div className="error"> All fields are required</div> }
-                                    { remoteError && <div className="error"> We are unable to process your request. <br/> {remoteError} </div> }
-                                    <StyledInput
-                                        type="text"
-                                        name="name-adress"
-                                        placeholder="Location Name or Address "
-                                        id="location-name-field"
-                                    >
-                                    </StyledInput>
-                                    <StyledTextarea 
-                                    rows="5"
-                                    placeholder="What do you think about this place?"
-                                    value={description}
-                                    onChange={handleChange}
-                                    >
-                                    </StyledTextarea>
-                                    <StyledButton type="submit">
-                                    {
-                                        loading 
-                                        ? 
-                                        <Loader type="Oval" color="#56C1CB" height={17} width={73} /> 
-                                        : 
-                                        <>Add Place</>
-                                    }
-                                    </StyledButton>
-                                </div>
-                                </Row>       
-                            )
-                        }
-                    </form>
-                    </>    
-                )
-            }
+  const handleChange = event => {
+    setDescription(event.target.value);
+    // if any error is being displayed, we remove it if user type in again
+    setFormError(false);
+  };
+
+  const redirectToLogin = () => {
+    props.history.push('/login');
+  };
+
+  // useEffect to use the Map place API autocomplete
+  useEffect(() => {
+    Promise.resolve(mapPromise).then(mapObject => {
+      const autocomplete = new mapObject.maps.places.Autocomplete(
+        document.getElementById('location-name-field')
+      );
+      // force auto-complete to return only business
+      autocomplete.setTypes(['establishment']);
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace();
+        if (place.photos && place.photos[0]) {
+          setLocationPhotos(place.photos[0].getUrl());
+        } else {
+          // no photo was received from place API. We set random picture from Unsplash
+          // but we dont display on the form
+          setLocationPhotos('https://source.unsplash.com/1600x900/?coworking');
+          setFromUnsplash(true);
+        }
+
+        setPlaceData(place);
+      });
+    });
+  });
+
+  return (
+    <StyledOverlayPopup isShown={isShown} id="add-location-form">
+      <div className="message-container">
+        <div className="closing-cross">
+          <span onClick={hideMessage}>X</span>
         </div>
-        </StyledOverlayPopup>
-    );
-};
+        {!isAuth && (
+          <StyledModal isShown={isShown}>
+            <span onClick={hideMessage}>&times;</span>
+            <div>
+              <h2>You must login first</h2>
+            </div>
+            <button onClick={redirectToLogin}>OK</button>
+          </StyledModal>
+        )}
+        {isAuth && (
+          <>
+            <form onSubmit={submitLocation}>
+              <div>
+                <h2>Add a working place</h2>
+              </div>
+              {isAdded ? (
+                <>
+                  <p> Thank you for adding this place </p>
+                  <StyledButton onClick={hideMessage}>OK</StyledButton>
+                </>
+              ) : (
+                <Row>
+                  {locationPhotos &&
+                    (!fromUnsplash && (
+                      <>
+                        <div className="left-box">
+                          <img
+                            className="location-photos"
+                            alt="location"
+                            src={locationPhotos}
+                          />
+                        </div>
+                      </>
+                    ))}
+                  <div className="right-box">
+                    {formError && (
+                      <div className="error"> All fields are required</div>
+                    )}
+                    {remoteError && (
+                      <div className="error">
+                        {' '}
+                        We are unable to process your request. <br />{' '}
+                        {remoteError}{' '}
+                      </div>
+                    )}
+                    <StyledInput
+                      type="text"
+                      name="name-adress"
+                      placeholder="Location Name or Address "
+                      id="location-name-field"
+                    ></StyledInput>
+                    <StyledTextarea
+                      rows="5"
+                      placeholder="What do you think about this place?"
+                      value={description}
+                      onChange={handleChange}
+                    ></StyledTextarea>
+                    <button type="submit">
+                      {loading ? (
+                        <Loader
+                          type="Oval"
+                          color="#56C1CB"
+                          height={17}
+                          width={73}
+                        />
+                      ) : (
+                        <>Add Place</>
+                      )}
+                    </button>
+                  </div>
+                </Row>
+              )}
+            </form>
+          </>
+        )}
+      </div>
+    </StyledOverlayPopup>
+  );
+}
 
 const mapStatetoProps = state => {
-    return {
-        loading: state.newLocation.loading,
-        remoteError: state.newLocation.error, 
-        isAdded: state.newLocation.isAdded,
-        isShown: state.newLocation.isShown,
-        isAuth: state.auth.userId,
-        geolocation: state.maps.geolocation,
-    };
+  return {
+    loading: state.newLocation.loading,
+    remoteError: state.newLocation.error,
+    isAdded: state.newLocation.isAdded,
+    isShown: state.newLocation.isShown,
+    isAuth: state.auth.userId,
+    geolocation: state.maps.geolocation
   };
-  
-  export default connect(
-    mapStatetoProps,
-    { addNewLocation, clearNewLocation, hideAddLocation, locationLoads }
-  )(AddLocation);
+};
+
+export default connect(
+  mapStatetoProps,
+  { addNewLocation, clearNewLocation, hideAddLocation, locationLoads }
+)(AddLocation);
--- src/components/componentStyles/FormElementsStyles.js
+++ src/components/componentStyles/FormElementsStyles.js
@@ -1,44 +1,54 @@
 import styled from 'styled-components';
 
-
 export const StyledButton = styled.button`
-    font-size: 1rem;
-    font-weight: 300;
-    margin: 0 10px;
-    padding: 15px;
-    margin-top: 15px;
-    background: none;
-    border: ${props =>
-    props.landing ? '0.5px solid #fff' : '0.5px solid #03525B'};
-    border-radius: 5px;
-    color: ${props => (props.landing ? '#fff' : '#666A7C')};
+  width: 100px;
+  height: 35px;
+  padding: 10px;
+  background-color: #56c1cb;
+  margin: 10px 0;
+  font-size: 1rem;
+  font-weight: 300;
+  border-radius: 5px;
+  color: #fff;
 `;
 
 export const StyledInput = styled.input`
-    width: 100%;
-    height: 40px;
-    background: #ffffff;
-    border: 1px solid #56c1cb;
-    box-sizing: border-box;
-    border-radius: 5px;
-    color: #666a7c;
-    padding-left: 10px;
-    margin: 10px 0;
+  width: 100%;
+  height: 40px;
+  background: #ffffff;
+  border: 1px solid #56c1cb;
+  box-sizing: border-box;
+  border-radius: 5px;
+  color: #666a7c;
+  padding-left: 10px;
+  margin: 10px 0;
 `;
 
 export const StyledTextarea = styled.textarea`
-    width: 100%;
-    background: #ffffff;
-    border: 1px solid #56c1cb;
-    box-sizing: border-box;
-    border-radius: 5px;
-    color: #666a7c;
-    padding-left: 10px;
-    margin: 10px 0;
-    outline: none;
+  width: 100%;
+  background: #ffffff;
+  border: 1px solid #56c1cb;
+  height: 130px;
+  border-radius: 5px;
+  outline: none;
+  resize: none;
+  margin: 10px 0;
+  outline: none;
+  padding: 10px;
+  font-family: 'Nunito', sans-serif;
+  font-size: 16px;
 `;
 
 export const Row = styled.div`
-    display:flex;
-    justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
+
+  button {
+    width: 100px;
+    height: 40px;
+    padding: 10px;
+    color: #fff;
+    background-color: #56c1cb;
+    border-radius: 5px;
+  }
 `;
